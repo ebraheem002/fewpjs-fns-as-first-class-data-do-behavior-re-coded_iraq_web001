6ef0471338aa7e18daf4fddc571c4398f97e3a99
Ebraheem Mahmood <ebraheemmahmood991@gmail.com>
2020-06-05 13:37:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,28 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+  let content = ''
+  let greeting = document.getElementById('greeting')
+function greet(timeString){
+  let conToArr = timeString.split(':')
+  let conToInt = parseInt(conToArr[0],10)
+  let conToInt1 = parseInt(conToArr[1],10)
+  if(conToInt<12){
+   return content = 'Good Morning'
+
+  }
+  if(conToInt>=12 && conToInt<=17){
+    
+         return content = 'Good Afternoon'
+  }
+  if(conToInt>17){
+    return content = 'Good Evening'
+
+  }
+
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(a){
+  greeting.textContent = content
+ 
+}
